b22374fe551ff34c04bcba87c40a74510df49bed
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-12 15:12:33 +0100

    Added error test if score inputted for roll1 or roll2 is not between 0 and 10

--- jasmine-standalone-3/spec/FrameSpec.js
+++ jasmine-standalone-3/spec/FrameSpec.js
@@ -10,12 +10,33 @@ describe('Frame', function() {
     expect(frame.roll1Points()).toEqual(null);
     expect(frame.roll2Points()).toEqual(null);
   });
-
-  it('can take and then set a score for roll1', function() {
-    frame.roll1(4);
-    expect(frame.roll1Points()).toEqual(4)
+  describe('roll1',function() {
+    it('can take a score as an argument and then set the score for roll1', function() {
+      frame.roll1(4);
+      expect(frame.roll1Points()).toEqual(4)
+    });
+    it('raises error if score inputted is not between 0 and 10 ', function() {
+      expect(function(){ frame.roll1(-2); }).toThrowError('Score not possible. Please input score between 0 and 10');
+    });
+    it('raises error if score inputted is not between 0 and 10 ', function() {
+      expect(function(){ frame.roll1(11); }).toThrowError('Score not possible. Please input score between 0 and 10');
+    });
   });
-  it('can take and and then set a score for roll2', function() {
+
+  describe('roll2', function() {
+    it('can take a score as an argument and then set the score for roll2', function() {
+      frame.roll2(5);
+      expect(frame.roll2Points()).toEqual(5)
+      });
+      it('raises error if score inputted is not between 0 and 10 ', function() {
+        expect(function(){ frame.roll2(-2); }).toThrowError('Score not possible. Please input score between 0 and 10');
+      });
+      it('raises error if score inputted is not between 0 and 10 ', function() {
+        expect(function(){ frame.roll2(11); }).toThrowError('Score not possible. Please input score between 0 and 10');
+      });
+  });
+
+  it('can take a score as an argument and then set the score for roll2', function() {
     frame.roll2(5);
     expect(frame.roll2Points()).toEqual(5)
     });
--- jasmine-standalone-3/src/Frame.js
+++ jasmine-standalone-3/src/Frame.js
@@ -7,9 +7,15 @@ class Frame {
   };
 
   roll1(number) {
+    if(number < 0 || number > 10) {
+      throw new Error('Score not possible. Please input score between 0 and 10')
+    };
     this._roll1 = number
   };
   roll2(number) {
+    if(number < 0 || number > 10) {
+      throw new Error('Score not possible. Please input score between 0 and 10')
+    };
     this._roll2 = number
   };
   roll1Points() {
