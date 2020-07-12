2f627781e7d30c60bd28172fc8129ec7bcf021d8
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-12 15:38:50 +0100

    Created minrollscore and maxrollscare constants to be used in the score for each roll error check.

--- jasmine-standalone-3/src/Frame.js
+++ jasmine-standalone-3/src/Frame.js
@@ -4,16 +4,18 @@ class Frame {
     this.frameScore = {}
     this._roll1 = null
     this._roll2 = null
+    this._MINROLLSCORE = 0
+    this._MAXROLLSCORE = 10
   };
 
   roll1(number) {
-    if(number < 0 || number > 10) {
+    if(number < this._MINROLLSCORE || number > this._MAXROLLSCORE) {
       throw new Error('Score not possible. Please input score between 0 and 10')
     };
     this._roll1 = number
   };
   roll2(number) {
-    if(number < 0 || number > 10) {
+    if(number < this._MINROLLSCORE || number > this._MAXROLLSCORE) {
       throw new Error('Score not possible. Please input score between 0 and 10')
     };
     this._roll2 = number
