179effe87449ac7e81ba6dbd1f070aabe8e0f858
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-12 19:44:58 +0100

    Can only add 10 frames to a game

--- jasmine-standalone-3/spec/ScorecardSpec.js
+++ jasmine-standalone-3/spec/ScorecardSpec.js
@@ -1,8 +1,27 @@
 'use strict';
 describe('ScoreCard', function() {
-var scorecard
-it('has an empty score array', function() {
-  scorecard = new ScoreCard();
-  expect(scorecard.game).toEqual([])
-});
+  var scorecard;
+  var frame1;
+  var frame2;
+
+  beforeEach(function() {
+    scorecard = new ScoreCard();
+  });
+
+  it('has an empty score array accessed by a method', function() {
+    expect(scorecard.gameScore()).toEqual([])
+  });
+  it('puts frame objects into the scorecard array', function() {
+    frame1 = jasmine.createSpyObj('frame',['roll1Points', 'roll2Points']);
+    frame2 = jasmine.createSpyObj('frame',['roll1Points', 'roll2Points']);
+    scorecard.addFrame(frame1);
+    scorecard.addFrame(frame2);
+    console.log(scorecard.gameScore())
+    expect(scorecard.gameScore().length).toEqual(2)
+  });
+  it('only accepts 10 frames/a full game', function() {
+    for (var i = 0; i<10; i++) {
+      scorecard.addFrame(frame1)};
+      expect(function(){ scorecard.addFrame(frame1); }).toThrowError('This game is now over')
+  });
 });
--- jasmine-standalone-3/src/ScoreCard.js
+++ jasmine-standalone-3/src/ScoreCard.js
@@ -4,5 +4,16 @@ class ScoreCard {
   this.game = []
   };
 
+  gameScore() {
+    return this.game
+  };
+
+  addFrame(frame) {
+    if((this.gameScore().length) >=10) {
+      throw new Error('This game is now over');
+    }
+    this.game.push(frame);
+  };
+
 };
 
